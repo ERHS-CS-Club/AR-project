09eb6f6bd00517c1941ee215ca0bb6aed49aa640
Viper4 <42948021+Viper4@users.noreply.github.com>
2023-06-06 11:48:14 -0500

    Smooth wheel rotation, locks based on distance from target angle

--- Scripts/Wheel.cs
+++ Scripts/Wheel.cs
@@ -30,30 +30,23 @@ public class Wheel : MonoBehaviour
         float angleRange = 360f / sections;
         for(int i = 0; i < sections; i++)
         {
-            Vector3 fixedEulers = wheelAnchor.localEulerAngles;
-            if(wheelAnchor.localEulerAngles.x > 180)
-            {
-                fixedEulers.x = wheelAnchor.localEulerAngles.x - 180;
-            }
-            if(wheelAnchor.localEulerAngles.y > 180)
-            {
-                fixedEulers.y = wheelAnchor.localEulerAngles.y - 180;
-            }
-            if(wheelAnchor.localEulerAngles.z > 180)
-            {
-                fixedEulers.z = wheelAnchor.localEulerAngles.z - 180;
-            }
-            float halfAngle = angleRange * 0.5f;
-            if(fixedEulers.z > angleRange * i - halfAngle && fixedEulers.z < angleRange * (i+1) - halfAngle)
+            float targetAngle = angleRange * i;
+            if (Mathf.Abs(wheelAnchor.localEulerAngles.z - targetAngle) < angleRange * 0.5f)
             {
                 selectedSection = i;
-                float targetAngle = angleRange * i;
-                while(Mathf.Abs(wheelAnchor.localEulerAngles.z - targetAngle) > 1)
-                {
-                    wheelAnchor.localEulerAngles = Vector3.RotateTowards(wheelAnchor.localEulerAngles, new Vector(wheelAnchor.localEulerAngles.x, wheelAnchor.localEulerAngles.y, targetAngle), Time.deltaTime * 5, 0);
-                }
+                StartCoroutine(LockWheel(targetAngle));
                 break;
             }
+        }
+    }
+
+    IEnumerator LockWheel(float targetAngle)
+    {
+        while (Mathf.Abs(wheelAnchor.localEulerAngles.z - targetAngle) > 1)
+        {
+            Debug.Log(Mathf.Abs(wheelAnchor.localEulerAngles.z - targetAngle));
+            wheelAnchor.localEulerAngles = Vector3.RotateTowards(wheelAnchor.localEulerAngles, new Vector3(wheelAnchor.localEulerAngles.x, wheelAnchor.localEulerAngles.y, targetAngle), Time.deltaTime * 150, Time.deltaTime * 150);
+            yield return new WaitForEndOfFrame();
         }
     }
 
